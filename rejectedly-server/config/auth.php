2fc28ebbe80a2df151dd8e0dcaf59023fa30f5f6
Reema224 <rima.abouchakra.gms@gmail.com>
2023-04-14 23:34:02 +0300

    added jwt authentication apis

--- rejectedly-server/config/auth.php
+++ rejectedly-server/config/auth.php
@@ -14,50 +14,25 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api',
         'passwords' => 'users',
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Guards
-    |--------------------------------------------------------------------------
-    |
-    | Next, you may define every authentication guard for your application.
-    | Of course, a great default configuration has been defined for you
-    | here which uses session storage and the Eloquent user provider.
-    |
-    | All authentication drivers have a user provider. This defines how the
-    | users are actually retrieved out of your database or other storage
-    | mechanisms used by this application to persist your user's data.
-    |
-    | Supported: "session"
-    |
-    */
 
     'guards' => [
         'web' => [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'api' => [
+                'driver' => 'jwt',
+                'provider' => 'users',
+        ],
+
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | User Providers
-    |--------------------------------------------------------------------------
-    |
-    | All authentication drivers have a user provider. This defines how the
-    | users are actually retrieved out of your database or other storage
-    | mechanisms used by this application to persist your user's data.
-    |
-    | If you have multiple user tables or models you may configure multiple
-    | sources which represent each model / table. These sources may then
-    | be assigned to any extra authentication guards you have defined.
-    |
-    | Supported: "database", "eloquent"
-    |
-    */
+
 
     'providers' => [
         'users' => [
--- rejectedly-server/database/migrations/2014_10_12_000000_create_users_table.php
+++ rejectedly-server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('username')->nullable();
+            $table->string('image_url')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->text('biography')->nullable();
+            $table->string('linkedin_url')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
