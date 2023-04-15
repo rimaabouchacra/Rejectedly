21c04ff24ba3231faf55b3e246646e3f33ff79c5
Reema224 <rima.abouchakra.gms@gmail.com>
2023-04-15 16:17:49 +0300

    eddited pages

---
+++ rejectedly-frontend/src/pages/loginpage/index.js
@@ -0,0 +1,12 @@
+import Navbar from "../../components/navbar";
+
+
+const Register=()=>{
+    return(
+        <div>
+          <Navbar/>
+          
+        </div>
+    )
+}
+export default Register;
