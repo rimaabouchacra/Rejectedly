e29d077da260db787b37d12c1bbd7a4561b40a44
Reema224 <rima.abouchakra.gms@gmail.com>
2023-04-16 01:10:28 +0300

    added rejection story style

--- rejectedly-frontend/src/components/rejectionstory/index.js
+++ rejectedly-frontend/src/components/rejectionstory/index.js
@@ -4,18 +4,16 @@ import '../index.css'
 const Story = ()=>{
 return (
     <div className="story">
+
       <div className='header'>
-        <h1>MY REJECTED STORIES</h1>
-        <button className='all-btn'>NEW STORY</button>
+          <h1>MY REJECTED STORIES</h1>
+          <button className='all-btn'>NEW STORY</button>
       </div>
-        
-      <ul>
-        <li><a href='stories'>MY STORIES</a></li>
-        <li><a href='profile'>EDIT PROFILE</a></li>
-        <li><a href='marketplace'>MARKETPLACE</a></li>
-        <li><a href='collaboration'>COLLABORATION</a></li><br/><br/><br/><br/><br/><br /><br />
-        <li className='li'><a href='/'>LOGOUT</a></li>
-      </ul>
+      <div className='text'>
+          <h2>NO STORIES YET</h2>
+          <p>Rejectedly gives you the opportunity to upload your rejection story for analysis using AI, so that you identify the points that need improvements. You will receive actionable suggestions on how to improve my approach, so that I can implement changes and see better results.</p>
+          <button className='all-btn'>NEW STORY</button>
+      </div>
     </div>
     );
 }
--- rejectedly-frontend/src/pages/rejectionanalysis/index.js
+++ rejectedly-frontend/src/pages/rejectionanalysis/index.js
@@ -1,9 +1,10 @@
 import Sidebar from "../../components/sidebar"
-
+import Story from "../../components/rejectionstory";
 const Analysis=()=>{
     return(
         <div>
             <Sidebar/>
+            <Story/>
         </div>
     )
 }
