e99282d87a6b27af039468fa82012c71f749536a
Reema224 <rima.abouchakra.gms@gmail.com>
2023-04-15 16:16:47 +0300

    added signup form

--- rejectedly-frontend/src/App.js
+++ rejectedly-frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Homepage from './pages/homepage';
+import Register from './pages/register';
 
 
 
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <Routes>
           <Route path="/" element={<Homepage/>} />
+          <Route path="/signup" element={<Register/>} />
       </Routes>  
     </Router> 
   
---
+++ rejectedly-frontend/src/pages/register/index.js
@@ -0,0 +1,12 @@
+import Navbar from "../../components/navbar";
+import Signup from "../../components/signup";
+
+const Register=()=>{
+    return(
+        <div>
+          <Navbar/>
+          <Signup/>
+        </div>
+    )
+}
+export default Register;
