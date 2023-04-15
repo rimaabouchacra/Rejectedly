3e4fa59dcfb72df42075b0d019467ce6149731c3
Reema224 <rima.abouchakra.gms@gmail.com>
2023-04-16 00:45:55 +0300

    added sidebar style

--- rejectedly-frontend/src/components/sidebar/index.js
+++ rejectedly-frontend/src/components/sidebar/index.js
@@ -5,12 +5,17 @@ const Sidebar = ()=>{
 
 return (
     <div className="sidebar">
+      
+      <div className='logo'>
         <img src={logo2} alt="logo" />
+      </div>
+        
       <ul>
         <li><a href='stories'>MY STORIES</a></li>
         <li><a href='profile'>EDIT PROFILE</a></li>
         <li><a href='marketplace'>MARKETPLACE</a></li>
-        <li><a href='collaboration'>COLLABORATION</a></li>
+        <li><a href='collaboration'>COLLABORATION</a></li><br/><br/><br/><br/><br/><br /><br />
+        <li className='li'><a href='/'>LOGOUT</a></li>
       </ul>
     </div>
     );
