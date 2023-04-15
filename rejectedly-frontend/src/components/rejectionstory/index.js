669d0333b80a103423a4b73fabf55038753bccf0
Reema224 <rima.abouchakra.gms@gmail.com>
2023-04-16 01:23:08 +0300

    added rejection story style final

--- rejectedly-frontend/src/components/rejectionstory/index.js
+++ rejectedly-frontend/src/components/rejectionstory/index.js
@@ -10,8 +10,8 @@ return (
           <button className='all-btn'>NEW STORY</button>
       </div>
       <div className='text'>
-          <h2>NO STORIES YET</h2>
-          <p>Rejectedly gives you the opportunity to upload your rejection story for analysis using AI, so that you identify the points that need improvements. You will receive actionable suggestions on how to improve my approach, so that I can implement changes and see better results.</p>
+          <h2 className='textt'>NO STORIES YET</h2>
+          <p className='textt'>Rejectedly gives you the opportunity to upload your rejection story for analysis using AI, so that you identify the points that need improvements. You will receive actionable suggestions on how to improve my approach, so that I can implement changes and see better results.</p>
           <button className='all-btn'>NEW STORY</button>
       </div>
     </div>
