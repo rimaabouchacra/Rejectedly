3696e0a7e7f6ee0f0e80749175381a5d5bdbe316
Reema224 <rima.abouchakra.gms@gmail.com>
2023-04-17 02:00:06 +0300

    added analysis section style

--- rejectedly-frontend/src/components/analysis/index.js
+++ rejectedly-frontend/src/components/analysis/index.js
@@ -1,17 +1,59 @@
-import './index.css'
-import '../index.css'
-import React from 'react';
-
+import React, { useState, useRef, useEffect } from 'react';
+import './index.css';
+import '../index.css';
 const Analysis = () => {
-return(
-    <div className='analysis'>
-        <h1>ANALYSIS</h1>
-        <label className='label2' htmlFor="type">Story type</label>
-        <div>
-            <p>Where does it come from? Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+  const [showAnalysis, setShowAnalysis] = useState(true);
+  const overlayRef = useRef(null);
+
+  function handleOverlayClick2(e) {
+    if (e.target === overlayRef.current) {
+      setShowAnalysis(false);
+      
+    }
+  }
+  
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOverlayClick2);
+    return () => {
+      document.removeEventListener('mousedown', handleOverlayClick2);
+    };
+  }, []);
+
+  return (
+    <>
+      {showAnalysis && (
+        <div className="analysis" ref={overlayRef}>
+          <h1>ANALYSIS</h1>
+          <h3 className='analysis-type'>
+            Analysis type
+          </h3>
+          <div>
+            <p>
+              Where does it come from? Contrary to popular belief, Lorem Ipsum
+              is not simply random text. It has roots in a piece of classical
+              Latin literature from 45 BC, making it over 2000 years old.
+              Richard McClintock, a Latin professor at Hampden-Sydney College
+              in Virginia, looked up one of the more obscure Latin words,
+              consectetur, from a Lorem Ipsum passage, and going through the
+              cites of the word in classical literature, discovered the
+              undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
+              1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good
+              and Evil) by Cicero, written in 45 BC. This book is a treatise
+              on the theory of ethics, very popular during the Renaissance.
+              The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..",
+              comes from a line in section 1.10.32.
+              
+            </p>
+          </div>
+          <button className="all-btn" onClick={() => setShowAnalysis(false)}>
+            CLOSE
+          </button>
+   
         </div>
-        <button className='all-btn'>CLOSE</button>
-    </div>
-)
-}
-export default Analysis
+      )}
+    </>
+  );
+};
+
+export default Analysis;
--- rejectedly-frontend/src/components/savedstories/index.js
+++ rejectedly-frontend/src/components/savedstories/index.js
@@ -3,6 +3,7 @@ import NewStory from '../newstory';
 import './index.css'
 import '../index.css'
 import '../rejectionstory/index.css'
+import Analysis from '../analysis';
 
 
 const Saved = () => {
@@ -26,17 +27,52 @@ const Saved = () => {
     };
   }, []);
 
+
+  const [showAnalysis, setShowAnalysis] = useState(false);
+  const overlayReff = useRef(null);
+
+  function handleButtonClick2() {
+    setShowAnalysis(true);
+  }
+
+  function handleOverlayClick2(e) {
+    if (e.target === overlayReff.current) {
+      setShowAnalysis(false);
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOverlayClick2);
+    return () => {
+      document.removeEventListener('mousedown', handleOverlayClick2);
+    };
+  }, []);
+
   return (
     <div className="story">
         <div className='header'>
           <h1>MY REJECTED STORIES</h1>
           <button onClick={handleButtonClick} className='all-btn'>NEW STORY</button>
+          {showStory && (
+        <div className='overlay' ref={overlayRef}>
+          <div className='popup'>
+            <NewStory />
+          </div>
+        </div>
+        )}
         </div>
         <div class="grid-container">
           <div class="grid-item">
             <h3>Story type</h3>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            <button className='analysis'><h3>SHOW ANALYSIS</h3></button>
+            <button onClick={handleButtonClick2} className='analysiss'><h3>SHOW ANALYSIS</h3></button>
+            {showAnalysis && (
+        <div className='overlay' ref={overlayReff}>
+          <div>
+            <Analysis />
+          </div>
+        </div>
+        )}
           </div>
           <div class="grid-item">2</div>
           <div class="grid-item">3</div>
