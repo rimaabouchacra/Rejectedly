8446c4765242b931241e60226c2c4dd4d1005edd
Reema224 <rima.abouchakra.gms@gmail.com>
2023-04-16 22:11:41 +0300

    edited profile page

--- rejectedly-frontend/src/components/profile/index.js
+++ rejectedly-frontend/src/components/profile/index.js
@@ -1,18 +1,55 @@
-import './index.css'
-import '../index.css'
-import '../rejectionstory/index.css'
+import React, { useState } from "react";
+import { FaCamera } from "react-icons/fa";
+import "./index.css";
+import "../index.css";
+import "../rejectionstory/index.css";
 
-const Profilee = () => {
-  
+const Profilee = ({ defaultImage }) => {
+  const [image, setImage] = useState(defaultImage);
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleImageChange = (e) => {
+    const selectedImage = URL.createObjectURL(e.target.files[0]);
+    setImage(selectedImage);
+  };
+
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+  };
 
   return (
     <div className="story">
-      <div className='header'>
-        
-      </div>
-
+        <div className="header">
+             <div
+      className="edit-profile-image"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className="profile-img">
+      <img src={image} alt="" className="profile-image" />  
+      </div>  
+      
+      {isHovering && (
+        <div className="edit-profile-image-overlay">
+          <label htmlFor="profile-image-upload">
+            <FaCamera />
+          </label>
+          <input
+            type="file"
+            id="profile-image-upload"
+            onChange={handleImageChange}
+          />
+        </div>
+      )}
     </div>
+        </div>
+    </div>
+ 
   );
-}
+};
 
 export default Profilee;
