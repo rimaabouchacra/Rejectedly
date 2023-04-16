9f68c682b2a4410cf8c4121c55c4afa865118d94
Reema224 <rima.abouchakra.gms@gmail.com>
2023-04-17 00:08:47 +0300

    edited profile page

--- rejectedly-frontend/src/components/editprofile/index.js
+++ rejectedly-frontend/src/components/editprofile/index.js
@@ -12,8 +12,34 @@ const EditProfile = () => {
         <h1>MY PROFILE</h1>
         <div className="profile-container">
             <Profilee/>
-            <div>
-                <input type="text" />
+            <div className="contact-inputs">
+                <div>
+                    <label className="profile-label" htmlFor="name">Full Name</label>
+                    <input type="text" />
+                </div>
+                <div>
+                    <label  className="profile-label"  htmlFor="username">Username</label>
+                    <input type="text" />
+                </div>
+                <div>
+                    <label  className="profile-label"  htmlFor="email">Email</label>
+                    <input type="email" />
+                </div>
+                <div>
+                    <label  className="profile-label"  htmlFor="phone">PhoneNumber</label>
+                    <input type="number" />
+                </div>
+            </div >
+            <div className="contact-inputs">
+                <div>
+                    <label  className="profile-label"  htmlFor="bio">Biography</label>
+                    <textarea name="bio" id="bio" cols="30" rows="10"></textarea>
+                </div>
+                <div>
+                    <label  className="profile-label"  htmlFor="linkedin">Linkedin</label>
+                    <input type="text" />
+                </div>
+
             </div>
         </div>
     </div>
