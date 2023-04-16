feeb1ee2e5ee2b7018566fcbcade9a23100ca771
Reema224 <rima.abouchakra.gms@gmail.com>
2023-04-16 20:07:42 +0300

    edited style

--- rejectedly-frontend/src/components/savedstories/index.js
+++ rejectedly-frontend/src/components/savedstories/index.js
@@ -36,6 +36,7 @@ const Saved = () => {
           <div class="grid-item">
             <h3>Story type</h3>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <button className='analysis'><h3>SHOW ANALYSIS</h3></button>
           </div>
           <div class="grid-item">2</div>
           <div class="grid-item">3</div>
