4865f47fd4483c2a12b2fa13e5b29e05ed71564a
Reema224 <rima.abouchakra.gms@gmail.com>
2023-04-20 23:24:35 +0300

    edited some feature

--- rejectedly-frontend/src/components/newgroup/index.js
+++ rejectedly-frontend/src/components/newgroup/index.js
@@ -3,6 +3,9 @@ import './index.css'
 import '../rejectionstory/index.css'
 import groupImg from '../../images/group.png'
 import search from '../../images/search.png'
+import comment from '../../images/comment.png'
+import send from '../../images/send.png'
+import arrow from '../../images/arrow.png'
 import '../newstory/index.css'
 import '../improvedstory/index.css'
 
@@ -27,13 +30,21 @@ const Group = () => {
             <label htmlFor="members">Group members</label>
             <div className='input type search'>
                 <img className="search-img" src={search} alt="search" />
-                <input className="input type search-text" placeholder="Search members to invite"></input>
+                <input className="input type search-text" placeholder="Search members to invite..."></input>
             </div>  
         </div>
         <div>
             <button className='all-btn'>SAVE</button>
         </div>
-        
+        <div className='input type search'>
+                <img className="search-img" src={comment} alt="cmnt" />
+                <input className="input type search-text" placeholder="Write a comment..."></input>
+                <img className="search-img" src={send} alt="send" />
+        </div>  
+        <div className="view">
+            <img src={arrow} alt="" />
+            <p>View comment</p>
+        </div>
     </div>
     </div>
   );
