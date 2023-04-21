d4405efcd88665ca80b250fda6a58e1c9aaaf9b9
Reema224 <rima.abouchakra.gms@gmail.com>
2023-04-22 00:26:31 +0300

    added comments model

--- rejectedly-server/app/Http/Controllers/UserController.php
+++ rejectedly-server/app/Http/Controllers/UserController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function store(Request $request, RejectionStory $story)
+    {
+        $validatedData = $request->validate([
+            'comment_text' => 'required|string|max:255',
+        ]);
+
+        $comment = new Comment();
+        $comment->comment_text = $validatedData['comment_text'];
+        $comment->user_id = auth()->user()->id;
+        $comment->story_id = $story->id;
+
+        $comment->save();
+
+        return redirect()->back();
+    }
 }
