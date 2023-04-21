6c2d0a564222ac36a6f0860d13100a111f9a57bc
Reema224 <rima.abouchakra.gms@gmail.com>
2023-04-21 23:24:18 +0300

    editded join group api

--- rejectedly-server/app/Http/Controllers/GroupController.php
+++ rejectedly-server/app/Http/Controllers/GroupController.php
@@ -53,7 +53,7 @@ class GroupController extends Controller
 
         return response()->json([
             'message' => 'Users added to group successfully'
-        ], 201);
+        ], 200);
 }
 
 
