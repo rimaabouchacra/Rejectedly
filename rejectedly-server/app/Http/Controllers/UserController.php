d34260fa69cd42fde73a8a6309c92bb9f3674cbd
Reema224 <rima.abouchakra.gms@gmail.com>
2023-04-22 21:10:03 +0300

    edited profile inputs

--- rejectedly-server/app/Http/Controllers/UserController.php
+++ rejectedly-server/app/Http/Controllers/UserController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Comment;
 use App\Models\RejectionStory;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Url;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -35,7 +38,7 @@ class UserController extends Controller
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
-    
+
     public function getComments(RejectionStory $story)
     {
        $comments = Comment::where('story_id', $story->id)->with('user')->get(['comment_text', 'user_id']);
@@ -52,6 +55,30 @@ class UserController extends Controller
             'comments' => $commentsWithUser,
         ]);
     }
+
+public function profile(Request $request)
+{
+
+$user_id = Auth::id();
+$user_updated = User::find($user_id);
+if (!$user_updated) {
+    return response()->json(['message' => 'User not found'], 404);
+}
+$user_updated->username = $request->username;
+$user_updated->image_url = $request->image_url;
+$user_updated->phone_number = $request->phone_number;
+$user_updated->biography = $request->biography;
+$user_updated->linkedin_url = $request->linkedin_url;
+$user_updated->save();
+return response()->json([
+    "status" => "success",
+    "updated_user" => $user_updated
+], 200);
+
+
+}
+
+
 
 
 
