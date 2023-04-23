720414c8795143d5e092b0726cf354486dbe1a7b
Reema224 <rima.abouchakra.gms@gmail.com>
2023-04-23 19:10:08 +0300

    edited post page

--- rejectedly-frontend/src/components/myposts/index.js
+++ rejectedly-frontend/src/components/myposts/index.js
@@ -23,6 +23,7 @@
 // }
 // export default Posts
 
+import './index.css'
 import '../index.css'
 import '../rejectionstory/index.css'
 import '../newstory/index.css'
@@ -33,7 +34,7 @@ import axios from 'axios';
 
 const Posts = () => {
   const [users, setUsers] = useState([]);
-  const [rejectionStories, setRejectionStories] = useState([]);
+  const [rejection_stories, setRejection_stories] = useState([]);
 
   useEffect(() => {
     axios.get('http://localhost:8000/api/v1/auth/user',
@@ -48,9 +49,12 @@ const Posts = () => {
         console.log(error);
       });
 
-    axios.get('http://localhost:8000/api/v1/auth/rejection-stories')
+    axios.get('http://localhost:8000/api/v1/auth/rejection-stories',
+    {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+    })
       .then(response => {
-        setRejectionStories(response.data);
+        setRejection_stories(response.data);
       })
       .catch(error => {
         console.log(error);
@@ -58,25 +62,31 @@ const Posts = () => {
   }, []);
 
   return (
-    <div className="story">
+    <div className="post-container">
       <div className='header collaborate'>
         <h1>COLLABORATIONS</h1>
         <button className='all-btn'>CREATE GROUP</button>
       </div>
-      <div className='new-story'>
+      <div className='post'>
         {users &&
           <div className='user-info'>
             <img src={users.image_url} alt='user' />
-            <h2>{users.name}</h2>
-            <p>{users.email}</p>
+            <div className='name-email'>
+                <h2>{users.name}</h2>
+                <p>{users.email}</p>
+            </div>
+            
           </div>
         }
-        {Array.isArray(rejectionStories) && rejectionStories.map(story => (
-  <div className='rejection-story' key={story.id}>
-    <h3>{story.type}</h3>
-    <p>{story.text}</p>
+        {/* {Array.isArray(rejection_stories) && rejection_stories.map(rejection_stories => ( */}
+  {rejection_stories&&
+  <div className='rejection-story' key={rejection_stories.id}>
+    <h3>hr</h3>
+    <h3>{rejection_stories.story_type}</h3>
+    <p>{rejection_stories.story_text}</p>
   </div>
-))}
+  }
+{/* ))} */}
 
       </div>
     </div>
