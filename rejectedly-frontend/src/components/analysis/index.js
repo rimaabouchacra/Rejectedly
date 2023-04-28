d0c7efc87a56908da0dfc807c005f49bad78cc5a
Reema224 <rima.abouchakra.gms@gmail.com>
2023-04-29 01:37:01 +0300

    frontend: send stories to ai

--- rejectedly-frontend/src/components/analysis/index.js
+++ rejectedly-frontend/src/components/analysis/index.js
@@ -94,7 +94,7 @@ const Analysis = ({analysisType, analysisText }) => {
       {showAnalysis && (
         <div className="analysis" ref={overlayRef}>
           <h1>ANALYSIS</h1>
-          <h3 className='analysis-type'>
+          <h3 className='analysis-typ'>
             {analysisType}
           </h3>
           <div>
