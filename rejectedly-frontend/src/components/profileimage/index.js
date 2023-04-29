1c6a59ace42cb0e77e6dd955dc11bd70eded52ef
Reema224 <rima.abouchakra.gms@gmail.com>
2023-04-29 22:29:53 +0300

    backend: edited edit profile

--- rejectedly-frontend/src/components/profileimage/index.js
+++ rejectedly-frontend/src/components/profileimage/index.js
@@ -1,64 +1,3 @@
-// import React, { useState } from "react";
-// import { FaCamera } from "react-icons/fa";
-// import "./index.css";
-// import "../index.css";
-// import "../rejectionstory/index.css";
-// import Profile from "../../images/profile.png";
-
-// const Profilee = ({ defaultImage }) => {
-//   const [image, setImage] = useState(defaultImage);
-//   const [isHovering, setIsHovering] = useState(false);
-
-//   const handleImageChange = (e) => {
-//     const selectedImage = URL.createObjectURL(e.target.files[0]);
-//     setImage(selectedImage);
-//   };
-
-//   const handleMouseEnter = () => {
-//     setIsHovering(true);
-//   };
-
-//   const handleMouseLeave = () => {
-//     setIsHovering(false);
-//   };
-
-//   return (
-//     <div>
-//         <div className="container">
-           
-//              <div
-//               className="edit-profile-image"
-//               onMouseEnter={handleMouseEnter}
-//               onMouseLeave={handleMouseLeave}
-//              >
-      
-//       <div className="profile-img">
-        
-//       <img src={image} alt="" className="profile-image" />
-//       <img src={Profile} alt="" />  
-     
-//       </div>  
-      
-//       {isHovering && (
-//         <div className="edit-profile-image-overlay">
-//           <label htmlFor="profile-image-upload">
-//             <FaCamera />
-//           </label>
-//           <input
-//             type="file"
-//             id="profile-image-upload"
-//             onChange={handleImageChange}
-//           />
-//         </div>
-//       )}
-//     </div>
-//         </div>
-//     </div>
- 
-//   );
-// };
-
-// export default Profilee;
 import React, { useState } from "react";
 import { FaCamera } from "react-icons/fa";
 import "./index.css";
@@ -120,3 +59,76 @@ const Profilee = ({ defaultImage }) => {
 };
 
 export default Profilee;
+// import React, { useState } from "react";
+// import { FaCamera } from "react-icons/fa";
+// import "./index.css";
+// import "../index.css";
+// import "../rejectionstory/index.css";
+// import Profile from "../../images/profile.png";
+
+
+// const Profilee = ({ defaultImage }) => {
+//   const [image_url, setImage] = useState(defaultImage);
+//   const [isHovering, setIsHovering] = useState(false);
+
+//   const handleImageChange = (e) => {
+//     const selectedImage = e.target.files[0];
+//     const reader = new FileReader();
+//     reader.readAsDataURL(selectedImage);
+//     reader.onloadend = () => {
+//       const base64String = reader.result;
+//       localStorage.setItem("profileImage", base64String);
+//       setImage(base64String);
+
+//       // Send image data to backend API
+//       const formData = new FormData();
+//       formData.append("profile_image", selectedImage);
+//       fetch("http://localhost:8000/api/v1/auth/profile", {
+//         method: "POST",
+//         body: formData,
+//       })
+//         .then((response) => response.json())
+//         .then((data) => console.log(data))
+//         .catch((error) => console.error(error));
+//     };
+//   };
+
+//   const handleMouseEnter = () => {
+//     setIsHovering(true);
+//   };
+
+//   const handleMouseLeave = () => {
+//     setIsHovering(false);
+//   };
+
+//   return (
+//     <div>
+//       <div className="container">
+//         <div
+//           className="edit-profile-image"
+//           onMouseEnter={handleMouseEnter}
+//           onMouseLeave={handleMouseLeave}
+//         >
+//           <div className="profile-img">
+//             <img src={image_url} alt="" className="profile-image" />
+//             <img src={Profile} alt="" />
+//           </div>
+//           {isHovering && (
+//             <div className="edit-profile-image-overlay">
+//               <label htmlFor="profile-image-upload">
+//                 <FaCamera />
+//               </label>
+//               <input
+//                 type="file"
+//                 id="profile-image-upload"
+//                 onChange={handleImageChange}
+//               />
+//             </div>
+//           )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Profilee;
--- rejectedly-server/app/Http/Controllers/UserController.php
+++ rejectedly-server/app/Http/Controllers/UserController.php
@@ -64,59 +64,59 @@ class UserController extends Controller
         ]);
     }
 
-// public function profile(Request $request)
-// {
-
-// $user_id = Auth::id();
-// $user_updated = User::find($user_id);
-// if (!$user_updated) {
-//     return response()->json(['message' => 'User not found'], 404);
-// }
-// $user_updated->username = $request->username;
-// $user_updated->image_url = $request->image_url;
-// $user_updated->phone_number = $request->phone_number;
-// $user_updated->biography = $request->biography;
-// $user_updated->linkedin_url = $request->linkedin_url;
-// $user_updated->save();
-// return response()->json([
-//     "status" => "success",
-//     "updated_user" => $user_updated
-// ], 200);
-
-
-// }
-
 public function profile(Request $request)
 {
-    $user_id = Auth::id();
-    $user_updated = User::find($user_id);
-    if (!$user_updated) {
-        return response()->json(['message' => 'User not found'], 404);
-    }
 
-    $user_updated->username = $request->username;
-    $user_updated->phone_number = $request->phone_number;
-    $user_updated->biography = $request->biography;
-    $user_updated->linkedin_url = $request->linkedin_url;
-
-    if ($request->has('profile_image')) {
-        // Decode the base64 string and save the image file to the server
-        $base64_image = $request->input('profile_image');
-        $image_data = base64_decode($base64_image);
-        $image_name = time() . '_' . uniqid() . '.png'; // Generate a unique image name
-        $path = public_path('/images/' . $image_name);
-        file_put_contents($path, $image_data);
-
-        // Save the image URL to the database
-        $user_updated->image_url = url('/images/' . $image_name);
-    }
-
-    $user_updated->save();
-    return response()->json([
-        "status" => "success",
-        "updated_user" => $user_updated
-    ], 200);
+$user_id = Auth::id();
+$user_updated = User::find($user_id);
+if (!$user_updated) {
+    return response()->json(['message' => 'User not found'], 404);
 }
+$user_updated->username = $request->username;
+$user_updated->image_url = $request->image_url;
+$user_updated->phone_number = $request->phone_number;
+$user_updated->biography = $request->biography;
+$user_updated->linkedin_url = $request->linkedin_url;
+$user_updated->save();
+return response()->json([
+    "status" => "success",
+    "updated_user" => $user_updated
+], 200);
+
+
+}
+
+// public function profile(Request $request)
+// {
+//     $user_id = Auth::id();
+//     $user_updated = User::find($user_id);
+//     if (!$user_updated) {
+//         return response()->json(['message' => 'User not found'], 404);
+//     }
+
+//     $user_updated->username = $request->username;
+//     $user_updated->phone_number = $request->phone_number;
+//     $user_updated->biography = $request->biography;
+//     $user_updated->linkedin_url = $request->linkedin_url;
+
+//     if ($request->has('profile_image')) {
+//         // Decode the base64 string and save the image file to the server
+//         $base64_image = $request->input('profile_image');
+//         $image_data = base64_decode($base64_image);
+//         $image_name = time() . '_' . uniqid() . '.png'; // Generate a unique image name
+//         $path = public_path('/images/' . $image_name);
+//         file_put_contents($path, $image_data);
+
+//         // Save the image URL to the database
+//         $user_updated->image_url = url('/images/' . $image_name);
+//     }
+
+//     $user_updated->save();
+//     return response()->json([
+//         "status" => "success",
+//         "updated_user" => $user_updated
+//     ], 200);
+// }
 
 
 
