9a76b65cadf11f45faa27788d1a9cf3645604fdd
Reema224 <rima.abouchakra.gms@gmail.com>
2023-05-06 00:32:16 +0300

    frontend: removed profile image component

--- rejectedly-frontend/src/components/editprofile/index.js
+++ rejectedly-frontend/src/components/editprofile/index.js
@@ -1,10 +1,188 @@
+// import React, { useState, useEffect } from "react";
+// import { FaCamera } from "react-icons/fa";
+// import "./index.css";
+// import "../index.css";
+// import "../rejectionstory/index.css";
+// import "../newstory/index.css";
+// import Profilee from "../profileimage";
+
+// const EditProfile = () => {
+//   const [username, setUsername] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [phone_number, setPhone] = useState("");
+//   const [biography, setBio] = useState("");
+//   const [linkedin_url, setLinkedin] = useState("");
+//   const [image_url, setImage] = useState("");
+//   const [DataSent, setDataSent] = useState(false);
+//   const [ErrorSent, setErrorSent] = useState(false);
+
+//   useEffect(() => {
+//     const storedProfile = JSON.parse(localStorage.getItem("profile")) || {};
+//     const user_id = localStorage.getItem("user_id");
+//     const token = localStorage.getItem("token");
+
+//     if (!token || !user_id) {
+//       console.error("No token or user_id found in local storage");
+//       return;
+//     }
+
+//     fetch(`http://localhost:8000/api/v1/auth/profile/${user_id}`, {
+//       method: "GET",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//     })
+//       .then((response) => response.json())
+//       .then((data) => {
+//         console.log("Profile data retrieved:", data);
+//         setUsername(data.username || "");
+//         setEmail(data.email || "");
+//         setPhone(data.phone_number || "");
+//         setBio(data.biography || "");
+//         setLinkedin(data.linkedin_url || "");
+//         setImage(data.image_url || "");
+//       })
+//       .catch((error) => {
+//         console.error("Error retrieving profile data:", error);
+//       });
+//   }, []);
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     const profileData = {
+//       username,
+//       email,
+//       phone_number,
+//       biography,
+//       linkedin_url,
+//       image_url,
+//     };
+
+//     try {
+//       const token = localStorage.getItem("token");
+//       const user_id = localStorage.getItem("user_id");
+
+//       if (!token || !user_id) {
+//         console.error("No token or user_id found in local storage");
+//         return;
+//       }
+
+//       const response = await fetch(`http://localhost:8000/api/v1/auth/profile`, {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//           Authorization: `Bearer ${token}`,
+//         },
+//         body: JSON.stringify(profileData),
+//       });
+
+//       const data = await response.json();
+//       console.log("Profile data saved:", profileData);
+
+//       localStorage.setItem("profile", JSON.stringify(profileData));
+//       setImage(data.image_url);
+//       setDataSent(true);
+//       setTimeout(() => {
+//         setDataSent(false);
+//       }, 3000);
+//     } catch (error) {
+//       console.error("Error saving profile data:", error);
+//       setErrorSent(true);
+//       setTimeout(() => {
+//         setErrorSent(false);
+//       }, 3000);
+//     }
+//   };
+
+
+//   return (
+//   <form className="story edit-profile" onSubmit={handleSubmit}>
+//     <h1 className="myprofile">MY PROFILE</h1>
+//     <div className="profile-container">
+//       <Profilee src={image_url}/>
+
+
+//       <div className="contact-inputs">
+//         <div>
+//           <label className="profile-label" htmlFor="username">
+//             Username
+//           </label>
+//           <input
+//             type="text"
+//             value={username}
+//             onChange={(e) => setUsername(e.target.value)}
+//           />
+//         </div>
+//         <div>
+//           <label className="profile-label" htmlFor="email">
+//             Email
+//           </label>
+//           <input
+//             type="email"
+//             value={email}
+//             onChange={(e) => setEmail(e.target.value)}
+//           />
+//         </div>
+//         <div>
+//           <label className="profile-label" htmlFor="phone">
+//             PhoneNumber
+//           </label>
+//           <input
+//             type="number"
+//             value={phone_number}
+//             onChange={(e) => setPhone(e.target.value)}
+//           />
+//         </div>
+//       </div>
+//       <div className="contact-inputs">
+//         <div>
+//           <label className="profile-label" htmlFor="bio">
+//             Biography
+//           </label>
+//           <textarea
+//             className="textarea"
+//             name="bio"
+//             id="bio"
+//             cols="30"
+//             rows="6"
+//             value={biography}
+//             onChange={(e) => setBio(e.target.value)}
+//           ></textarea>
+//         </div>
+//         <div className="spacing">
+//           <label className="profile-label" htmlFor="linkedin">
+//             Linkedin
+//           </label>
+//           <input
+//             type="text"
+//             value={linkedin_url}
+//             onChange={(e) => setLinkedin(e.target.value)}
+//           />
+//         </div>
+//         <div>
+//           <button className="all-btn save" type="submit">
+//             SAVE
+//           </button>
+//           {DataSent  && <p>Saved successfully!</p>}
+//           {ErrorSent  && <p>Failed to save! try to change image size</p>}
+          
+//         </div>
+//       </div>
+//     </div>
+//   </form>
+// )};
+// export default EditProfile
+
+
 import React, { useState, useEffect } from "react";
 import { FaCamera } from "react-icons/fa";
 import "./index.css";
 import "../index.css";
 import "../rejectionstory/index.css";
 import "../newstory/index.css";
-import Profilee from "../profileimage";
+import Profile from "../../images/profile.png";
 
 const EditProfile = () => {
   const [username, setUsername] = useState("");
@@ -12,11 +190,13 @@ const EditProfile = () => {
   const [phone_number, setPhone] = useState("");
   const [biography, setBio] = useState("");
   const [linkedin_url, setLinkedin] = useState("");
+  const [image_url, setImage] = useState("");
   const [DataSent, setDataSent] = useState(false);
   const [ErrorSent, setErrorSent] = useState(false);
-
+  
+  const [isHovering, setIsHovering] = useState(false);
+  
   useEffect(() => {
-    const storedProfile = JSON.parse(localStorage.getItem("profile")) || {};
     const user_id = localStorage.getItem("user_id");
     const token = localStorage.getItem("token");
 
@@ -40,6 +220,7 @@ const EditProfile = () => {
         setPhone(data.phone_number || "");
         setBio(data.biography || "");
         setLinkedin(data.linkedin_url || "");
+        setImage(data.image_url || "");
       })
       .catch((error) => {
         console.error("Error retrieving profile data:", error);
@@ -55,7 +236,7 @@ const EditProfile = () => {
       phone_number,
       biography,
       linkedin_url,
-      image_url: localStorage.getItem("profileImage"),
+      image_url,
     };
 
     try {
@@ -80,6 +261,7 @@ const EditProfile = () => {
       console.log("Profile data saved:", profileData);
 
       localStorage.setItem("profile", JSON.stringify(profileData));
+    
       setDataSent(true);
       setTimeout(() => {
         setDataSent(false);
@@ -93,14 +275,59 @@ const EditProfile = () => {
     }
   };
 
+    const handleImageChange = (e) => {
+    const selectedImage = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(selectedImage);
+    reader.onloadend = () => {
+      const base64String = reader.result;
+      localStorage.setItem("profileImage", base64String);
+      setImage(base64String);
+    };
+  };
+
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+  };
+
   return (
   <form className="story edit-profile" onSubmit={handleSubmit}>
     <h1 className="myprofile">MY PROFILE</h1>
     <div className="profile-container">
-      <Profilee/>
+      <div>
+      {/*Profile image   */}
+      <div className="container">
+        <div
+          className="edit-profile-image"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <div className="profile-img">
+            <img src={image_url} alt="" className="profile-image" />
+            <img src={Profile} alt="" />
+          </div>
+          {isHovering && (
+            <div className="edit-profile-image-overlay">
+              <label htmlFor="profile-image-upload">
+                <FaCamera />
+              </label>
+              <input
+                type="file"
+                id="profile-image-upload"
+                onChange={handleImageChange}
+              />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+    {/* Profile image */}
 
-
-      <div className="contact-inputs">
+    <div className="contact-inputs">
         <div>
           <label className="profile-label" htmlFor="username">
             Username
@@ -131,7 +358,7 @@ const EditProfile = () => {
             onChange={(e) => setPhone(e.target.value)}
           />
         </div>
-      </div>
+    </div>
       <div className="contact-inputs">
         <div>
           <label className="profile-label" htmlFor="bio">
